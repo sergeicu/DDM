13b8fd37f3c29aa8f7b72d6ec3cda99658784f32
Serge Vasylechko <serge.vasylechko@childrens.harvard.edu>
2024-02-21 12:29:42 -0500

    graceful exit from test files + fixed bugs in symlinking (need to retrain OE database)

--- preprocessing/acdc_symlink.py
+++ preprocessing/acdc_symlink.py
@@ -1,41 +1,39 @@
-# todo: 
-# do not multiply the affine - it should stay the same - not increase in size... 
-# need to downsample by 2x in x,y plane to reach 128x128 pixels...
+# link files 
+
+
+# currently done 
+    # python preprocessing/acdc_symlink.py data_ES_motion_noOE mat_oe_motion_noOE
+    # python preprocessing/acdc_symlink.py data_ES_motion_OE mat_oe_motion_OE
+    # python preprocessing/acdc_symlink.py data_ES_nomotion_OE mat_oe_nomotion_OE
+    # python preprocessing/acdc_symlink.py data_ES_nomotion_noOE mat_oe_nomotion_noOE
+    # python preprocessing/acdc_symlink.py data_ED_ES mat
+
 
 
 import glob 
 import os 
+import sys 
+from IPython import embed
+
+savedir_target = sys.argv[1] # 'data_ED_ES' -> 
+savedir_source = sys.argv[2] # 'mat' -> /fileserver/Rad-Warfield-e2/Groups/Imp-Recons/serge/code/diffusion/experiments/s20240209_oecorr/DDM/data/acdc/
+assert len(sys.argv) ==3 
+
 
 d='/fileserver/Rad-Warfield-e2/Groups/Imp-Recons/serge/code/diffusion/experiments/s20240209_oecorr/DDM/data/acdc/'
-
-savedir_r = d + 'data_ED_ES/'
+savedir_r = d + savedir_target + "/" #'data_ED_ES/'
 os.makedirs(savedir_r, exist_ok=True)
 
-
-
-from IPython import embed; 
-
-
-split = [76, 10, 10]
-
-# for folder in ['training', 'testing']:
 for folder in ['training', 'testing']:
     os.chdir(d+folder)
     patients = sorted(glob.glob('patient*/'))
     for c, patient in enumerate(patients): 
         
-        savematdir = patient + "mat/"
-        savename_iso = savematdir + patient.replace("/", "_iso.mat")
-        
-        assert os.path.exists(savename_iso), embed()
-        
-        # # create dir
-        # if c <76: 
-        #     savedir = d + "train/"
-        # elif c >= 76 and c < 86:
-        #     savedir = d + "val/"
-        # elif c >= 86: 
-        #     savedir = d + "test/"
+        savematdir = patient + savedir_source + "/"
+        assert os.path.exists(savematdir)
+        matfiles = glob.glob(savematdir + "*.mat")
+        assert len(matfiles) ==1, embed()
+        savename_iso = matfiles[0]
             
         os.makedirs(savedir_r + folder.replace("ing", "/"), exist_ok=True)
         newfilename = savedir_r + folder.replace("ing", "/") + os.path.basename(savename_iso)
