d2ffd6efc2aac86589fd5b64d321a199bf62544b
Serge Vasylechko <serge.vasylechko@childrens.harvard.edu>
2024-02-23 16:05:46 -0500

    added test config file but test itself is still not implemented

--- exp1.sh
+++ exp1.sh
@@ -37,6 +37,9 @@ git checkout ddpm_only
 python3 DDM_train.py -p train -c config/DDM_train_b8_ddpm_only.json
 
 
+# [not implemented at all] test ddpm only 
+python3 DDM_test.py -p test -c config/DDM_test_b8_ddpm_only.json # experiments/DDM_test_240221_152036/results
+
 # start on e2 
 e2 
 srun -A crl -p crl-gpu -t 14:00:00 --qos=crl --gres=gpu:NVIDIA_A40:1 -n 8 --mem 128GB --pty /bin/bash 
